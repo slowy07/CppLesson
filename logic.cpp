e9c5664595b66cb3ad63917fe27181f46e0352c0
slowy07 <40540262+slowy07@users.noreply.github.com>
2020-06-02 00:43:20 +0700

    Create logic.cpp

---
+++ logic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+  bool isMale = true;
+  if(isMale == true)
+  {
+    cout<<"you are male";
+  }
+  else
+  {
+    cout<<"you are male";
+  }
+  
+  return 0;
+}
