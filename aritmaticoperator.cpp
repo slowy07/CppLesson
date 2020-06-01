ac719983699128218c42b47644db145f8c680506
slowy07 <40540262+slowy07@users.noreply.github.com>
2020-06-02 00:19:57 +0700

    Update aritmaticoperator.cpp

--- aritmaticoperator.cpp
+++ aritmaticoperator.cpp
@@ -5,15 +5,17 @@ using namespace std;
 
 int main()
 {
-  int angka1,angka2;
+  int angka1,angka2,hasil,hasil2;
   angka1= 12;
   angka2= 12;
+  hasil2 = angka1+angka2*13;
   hasil = pow(angka1,angka2);
   cout<<angka1+angka2;
   cout<<angka1/angka2;
   cout<<angka1*angka2;
   cout<<angka1%angka2;
   cout<<hasil;
+  cout<<hasil2;
   
   
   return 0;
