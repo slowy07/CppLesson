d5a2dec85c73d33b4e75eafe7e08ee5e6b06590f
slowy07 <40540262+slowy07@users.noreply.github.com>
2020-06-02 00:46:46 +0700

    Update logic.cpp

--- logic.cpp
+++ logic.cpp
@@ -5,9 +5,14 @@ using namespace std;
 int main()
 {
   bool isMale = true;
+  bool isjomblo = true;
   if(isMale == true)
   {
     cout<<"you are male";
+    if(isjomblo = true)
+    {
+      cout<<"jomblo";
+    }
   }
   else
   {
