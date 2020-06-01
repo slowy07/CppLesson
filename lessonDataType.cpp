cfdb732f74e3f2ea6688668f5ed557120d50e605
slowy07 <40540262+slowy07@users.noreply.github.com>
2020-06-01 21:32:01 +0700

    Create lessonDataType.cpp

---
+++ lessonDataType.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int integerNum = 12; //datatype with integer number 
+    float floatingNumber = 3.0; //datatype with floating point number
+    char character = 'a'; //datatype with single character
+    string characterString = "zulkepret" //datatype wiith strings value 
+
+    cout<<integerNum<<endl; //output will show integer number 12
+    cout<<15<<endl; //output will show integer   number 15
+    cout<<floatingNumber<<endl; //output will show  floating point 3.0
+    cout<<14.5<<endl; //output will show floating point number 14.5
+    cout<<character<<endl; //output will show character
+    cout<<'F'<<endl; //output will show charcater
+    cout<<characterString<<endl; //output will show string
+    cout<<"welcome"<<endl; //output will show string
+}
