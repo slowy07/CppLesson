10becfe04251b6ab32bbd8ef63cbcda9b95a7000
arfy slowy <40540262+slowy07@users.noreply.github.com>
2020-06-02 23:21:15 +0700

    Create CombineTwoStringCpp.cpp

---
+++ CombineTwoStringCpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std;
+
+
+int main()
+{
+    string firstname = "arfy";
+    string secondname = "slowy";
+    string fullname = firstname + secondname;
+    //string fullnameAppend = firstname.append(secondname); //other combine btween name
+    
+    cout<<fullname;
+    
+    return 0;
+}
