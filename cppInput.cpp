81c4f8478c8a089ecbddcc4faa16b5d5aa53b17a
slowy07 <40540262+slowy07@users.noreply.github.com>
2020-06-02 20:57:38 +0700

    Update cppInput.cpp

--- cppInput.cpp
+++ cppInput.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+
+int main()
+{
+  //input using c input (cin) on c++
+  string name;
+  int age;
+  cout<<"input name = "; cin>>name;
+  cout<<"input age = "; cin>>age;
+  cout<<"hello "<<name<<endl;
+  cout<<"you age "<<age<<" years old!"<<endl;
+  return 0;
+}
