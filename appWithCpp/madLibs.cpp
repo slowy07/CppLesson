cc89781bd8f3efc5c58b8eb3b8db89b25b57851b
slowy07 <40540262+slowy07@users.noreply.github.com>
2020-06-02 21:31:40 +0700

    Create madLibs.cpp

---
+++ appWithCpp/madLibs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string color1,color2,endText;
+  //madlibs game
+  cout<<"madlibs game"<<endl;
+  cout<<"insert first color = "; cin>>color1;
+  cout<<"insert second color = "; cin>>color2;
+  cout<<"enter phrase = "; cin>>endText;
+  
+  
+ //output
+  cout<<"roses are "<<color1<<endl;
+  cout<<"violets are "<<color2<<endl;
+  cout<<endText<<endText<<endl;
+  return 0;
+}
