60d3b0ddfa1b623f819a3415ecdc551d064e1f1b
slowy07 <40540262+slowy07@users.noreply.github.com>
2020-06-02 21:24:10 +0700

    Update logicWithSwitch.cpp

--- logicWithSwitch.cpp
+++ logicWithSwitch.cpp
@@ -7,5 +7,15 @@ int main()
   int day = 3;
   switch(day)
   {
-    case
+    case 1:
+      cout<<"monday"<<endl;
+      break;
+    case 2:
+      cout<<"tuesday"<<endl;
+      break;
+    case 3:
+      cout<<"wednesday"<<endl;
+      break;
+  }
+  return 0;
 }
