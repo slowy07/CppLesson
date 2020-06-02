3079d2aa45e5124b20f521ec3ef79edad551d541
arfy slowy <40540262+slowy07@users.noreply.github.com>
2020-06-02 23:26:54 +0700

    Create moreAboutString.cpp

---
+++ moreAboutString.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    string name = "arfyslowy";
+    cout<<name[0]<<endl; //output will show A
+    //change string 
+    //name[3] = 'i' //arfislowy
+    
+    //length of string 
+    cout<<name.length(); //you can use name.size();
+ }
