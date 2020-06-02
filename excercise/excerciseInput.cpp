70457e0c9bc99f536168d701d9b313ace98e0b8c
slowy07 <40540262+slowy07@users.noreply.github.com>
2020-06-02 22:22:02 +0700

    Create excerciseInput.cpp

---
+++ excercise/excerciseInput.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    //fix the error
+    double inputNumberFirst,inputNumberSecond;
+    double sum;
+
+
+    cout<<"insert number = "; cin>>>inputNumber;
+    cout<<"insert second number"; cin>>>inputNumberSecond;
+    
+    sum = inputNumberFirst+inputNumberSecond;
+    cout<<"sum is =  "<<sum<<endl;
+    
+
+    return 0;
+}
