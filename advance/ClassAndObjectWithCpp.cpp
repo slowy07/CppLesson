25608612ee0fb4d36c4aef067519176c2cea2fe9
arfy slowy <40540262+slowy07@users.noreply.github.com>
2020-06-02 22:55:47 +0700

    Update ClassAndObjectWithCpp.cpp

--- advance/ClassAndObjectWithCpp.cpp
+++ advance/ClassAndObjectWithCpp.cpp
@@ -10,8 +10,10 @@ class car
 
 int main()
 {
+    //create object
     car objCar;
     
+    //create value of class 
     objCar.sumCar = 1;
     objCar.nameCar = "ferrari";
 
@@ -20,5 +22,5 @@ int main()
     cout<<objCar.sumCar<<endl;
     cout<<objCar.nameCar<<endl;
     
-
+    return 0;
 }
