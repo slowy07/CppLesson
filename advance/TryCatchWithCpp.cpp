1a025452db8c83f7feeb64665969c356d95e9d5d
arfy slowy <40540262+slowy07@users.noreply.github.com>
2020-06-05 22:12:00 +0700

    Update TryCatchWithCpp.cpp

--- advance/TryCatchWithCpp.cpp
+++ advance/TryCatchWithCpp.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+  try
+  {
+    int length,width,sum;
+    if(!(cin>>length))
+      throw length;
+    cout<<"length = "; cin>>length;
+    if(!(cin>>width))
+      throw width;
+    cout<<"width = "; cin>>width;
+    sum = length * width;
+    cout<<"sum = "<<sum<<endl;
+  }
+  catch(int error)
+  {
+    cout<<"input eror ! number allowed !"<<endl;
+  }
+  return 0;
+}
