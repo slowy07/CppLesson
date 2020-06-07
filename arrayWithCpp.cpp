bcb96e89b01372020bbd9f6583ca8d44b20b1212
arfy slowy <40540262+slowy07@users.noreply.github.com>
2020-06-07 20:05:05 +0700

    Update arrayWithCpp.cpp

--- arrayWithCpp.cpp
+++ arrayWithCpp.cpp
@@ -18,14 +18,20 @@ int main()
     };
 
     //array start from 0 = mercedes benz
-    cout<<cars<<endl;
+    for(int array_cars; array_cars<=5; aray_cars++)
+    {
+        cout<<cars<<endl;
+    }
 
     //for make spesification output
     cout<<cars[0]<<endl; //output mercedes benz
 
     //change array element
     country[1] = "united states"; //united kingdom change to united states
-    cout<<country<<endl; 
+    for(array_country; array_country<=5; array_country++)
+    {
+        cout<<country<<endl;
+    } 
     return 0;
 
 }
