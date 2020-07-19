bfd73e37b0d9bf88a3a8d202cdc8a1dc66a93d6f
arfy slowy <40540262+slowy07@users.noreply.github.com>
2020-07-19 22:41:16 +0700

    Update secondCallotherFile.cpp

--- moreAdvance/secondCallotherFile.cpp
+++ moreAdvance/secondCallotherFile.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+
+void displayMenu()
+{
+  cout<<"+ = add"<<endl;
+  cout<<"- = minus"<<endl;
+  cout<<"* = multiple"<<endl;
+  cout<<"/ = divide"<<endl;
+  cout<<"^ = exponent"<<endl;
+}
+
+void sumAddnumber(double num1, double num2)
+{
+  return num1+num2;
+}
+void sumMinusnumber(double num1, double num2)
+{
+  return num1-num2;
+}
+void sumMultiplenumber(double num1, double num2)
+{
+  return num1*num2;
+}
+void sumDividenumber(double num1, double num2)
+{
+  return num1/num2;
+}
+void sumExponentnumber(double num1, double num2)
+{
+  return pow(num1,num2);
+}
