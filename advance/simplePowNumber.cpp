2b1119f755af1b08657d5f403d54afe1f094f731
arfy slowy <40540262+slowy07@users.noreply.github.com>
2020-07-20 01:53:17 +0700

    Update simplePowNumber.cpp

--- advance/simplePowNumber.cpp
+++ advance/simplePowNumber.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+
+int getPowNumber(int num1, int num2);
+{
+  int result=1;
+  for(int i=0; i<num2; i++)
+  {
+    result *= num1;
+  }
+  return result;
+}
+int main()
+{
+  int number1,number2;
+  cout<<"input number = "; cin>>number1;
+  cout<<"input number 2 = "; cin>>number2;
+  cout<<getPowerNumber(number1,number2)<<endl;
+  return 0;
+}
