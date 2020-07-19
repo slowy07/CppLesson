b40f703e7fe7cf70bbea1012ad25a8538d40f595
arfy slowy <40540262+slowy07@users.noreply.github.com>
2020-07-19 22:48:03 +0700

    Update callotherFile.cpp

--- moreAdvance/callotherFile.cpp
+++ moreAdvance/callotherFile.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <cmath>
 #include secondCallotherFile.cpp
 using namespace std;
 
 
 int main()
 {
+  char operation;
+  double number1,number2;
+  displayMenu();
+  
+  cout<<"input number:" ; cin>>number1;
+  cout<<"input number2:" ; cin>>number2;
+  cout<<"operation = "; cin>>operation;
+  
+  switch(operation)
+  {
+    case '+':
+      cout<<"Sum :"<<sumAddnumber(number1, number2)<<endl;
+     break;
+    case '-':
+      cout<<"Sum :"<<sumMinusnumber(number1, number2)<<endl;
+      break;
+    case '*':
+      cout<<"Sum :"<<sumMultiplenumber(number1, number2)<<endl;
+      break;
+    case '/':
+      cout<<"Sum :"<<sumAddnumber(number1, number2)<<endl;
+      break;
+    case '^':
+      cout<<"Sum :"<<sumExponentnumber(number1, number2)<<endl;
+      break;
+    default:
+      cout<<"invalid operation !"<<endl;
+      break;
+  }
+  
   return 0;
 }
