d5be1af2a7adc4e0f88b5f44f2cd2be85a3fb426
arfy slowy <40540262+slowy07@users.noreply.github.com>
2020-11-10 12:08:10 +0700

    caesar chiper implementation in cpp

---
+++ cryptography/caesarChipper.cpp
@@ -0,0 +1,32 @@
+#include <iostream> 
+using namespace std; 
+
+string encrypt(string text, int s) 
+{ 
+	string result = ""; 
+
+	// traverse text 
+	for (int i=0;i<text.length();i++) 
+	{ 
+		// apply transformation to each character 
+		// Encrypt Uppercase letters 
+		if (isupper(text[i])) 
+			result += char(int(text[i]+s-65)%26 +65); 
+
+	else
+		result += char(int(text[i]+s-97)%26 +97); 
+	} 
+
+	// Return the resulting string 
+	return result; 
+} 
+
+int main() 
+{ 
+	string text="memelitlordmaulewat"; 
+	int s = 4; 
+	cout << "Text : " << text; 
+	cout << "\nShift: " << s; 
+	cout << "\nCipher: " << encrypt(text, s); 
+	return 0; 
+} 
