0a8f366978b39f21d223be0022de2d6ee9a81485
slowy07 <slowy.arfy@gmail.com>
2020-12-23 23:28:58 +0700

    add two number

---
+++ leetCodeSolution/addTwoNumber(medium)/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* a, ListNode* b) {
+        int carry = 0;
+        ListNode dummy, *tail = &dummy;
+        while (a || b || carry) {
+            if (a) {
+                carry += a->val;
+                a = a->next;
+            }
+            if (b) {
+                carry += b->val;
+                b = b->next;
+            }
+            tail->next = new ListNode(carry % 10);
+            tail = tail->next;
+            carry /= 10;
+        }
+        return dummy.next;
+    }
+};
