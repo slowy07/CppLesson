83c5c9a321e13196ac687458eaed7434466f84ee
slowy07 <slowy.arfy@gmail.com>
2020-12-26 12:33:55 +0700

    letter combination of phone number

---
+++ leetCodeSolution/letterCombinationOfPhoneNumber/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    vector<string> m{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    vector<string> ans;
+    void dfs(string &digits, int start, string &str) {
+        if (start == digits.size()) {
+            ans.push_back(str);
+            return;
+        }
+        for (char c : m[digits[start] - '2']) {
+            str.push_back(c);
+            dfs(digits, start + 1, str);
+            str.pop_back();
+        }
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+        string str;
+        dfs(digits, 0, str);
+        return ans;
+    }
+};
