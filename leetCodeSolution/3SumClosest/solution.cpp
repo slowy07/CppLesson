2b6dd2ae03d00a603992a9ea3547e84c3149321c
slowy07 <slowy.arfy@gmail.com>
2020-12-26 12:30:33 +0700

    3sum closest

---
+++ leetCodeSolution/3SumClosest/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& A, int target) {
+        sort(begin(A), end(A));
+        int diff = INT_MAX;
+        for (int i = 0, N = A.size(); i < N; ++i) {
+            int L = i + 1, R = N - 1;
+            while (L < R) {
+                int sum = A[L] + A[R] + A[i];
+                if (sum == target) return target;
+                if (abs(target - sum) < abs(diff)) diff = target - sum;
+                if (sum > target) --R;
+                else ++L;
+            }
+        }
+        return target - diff;
+    }
+};
