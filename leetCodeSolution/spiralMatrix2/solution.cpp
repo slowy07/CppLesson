af3bcc0848cd067bfaf5f7d3854b892d5a2bef8a
slowy07 <slowy.arfy@gmail.com>
2020-12-27 06:35:17 +0700

    spiral matrix 2 (easy)

---
+++ leetCodeSolution/spiralMatrix2/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> ans(n, vector<int>(n));
+        for (int i = 0, d = 1; i < n / 2; ++i) {
+            int len = n - 2 * i - 1;
+            for (int j = 0; j < len; ++j) ans[i][i + j] = d++;
+            for (int j = 0; j < len; ++j) ans[i + j][n - i - 1] = d++;
+            for (int j = 0; j < len; ++j) ans[n - i - 1][n - i - j - 1] = d++;
+            for (int j = 0; j < len; ++j) ans[n - i - j - 1][i] = d++;
+        }
+        if (n % 2) ans[n / 2][n / 2] = n * n;
+        return ans;
+    }
+};
