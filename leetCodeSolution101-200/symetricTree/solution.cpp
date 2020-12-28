1a7781cc3c7bdcb97ed87d51f62e8504dc3dd36d
slowy07 <slowy.arfy@gmail.com>
2020-12-28 22:54:12 +0700

    symetric tree (easy)

---
+++ leetCodeSolution101-200/symetricTree/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+private:
+  bool isSymmetric(TreeNode *r1, TreeNode *r2) {
+    return (!r1 && !r2)
+    || (r1 && r2 && r1->val == r2->val && isSymmetric(r1->left, r2->right) && isSymmetric(r1->right, r2->left));
+  }
+public:
+  bool isSymmetric(TreeNode* root) {
+    return isSymmetric(root, root);
+  }
+};
