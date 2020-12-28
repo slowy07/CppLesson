ca3110fa1e2e492906254185d4a85b0c23f62796
slowy07 <slowy.arfy@gmail.com>
2020-12-28 21:28:30 +0700

    recover binary search tree (hard)

---
+++ leetCodeSolution/recoverBinarySearchTree/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    TreeNode *a = NULL, *b = NULL;
+    void update(TreeNode *x, TreeNode *y) {
+        if (!a || x->val < a->val) a = x;
+        if (!b || y->val > b->val) b = y;
+    }
+    void dfs(TreeNode *root, TreeNode *left = NULL, TreeNode *right = NULL) {
+        if (!root) return;
+        if (left && left->val > root->val) update(root, left);
+        if (right && right->val < root->val) update(right, root);
+        dfs(root->left, left, root);
+        dfs(root->right, root, right);
+    }
+public:
+    void recoverTree(TreeNode* root) {
+        dfs(root);
+        swap(a->val, b->val);
+    }
+};
