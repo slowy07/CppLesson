b2643899e3534025d2cc541b27734901840e04ac
slowy07 <slowy.arfy@gmail.com>
2020-12-28 22:52:05 +0700

    binary tree level order tranversal (medium)

---
+++ leetCodeSolution101-200/binaryTreeLevelOrderTranversal/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if (!root) return {};
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (q.size()) {
+            int cnt = q.size();
+            ans.emplace_back();
+            while (cnt--) {
+                root = q.front();
+                q.pop();
+                ans.back().push_back(root->val);
+                if (root->left) q.push(root->left);
+                if (root->right) q.push(root->right);
+            }
+        }
+        return ans;
+    }
+};
