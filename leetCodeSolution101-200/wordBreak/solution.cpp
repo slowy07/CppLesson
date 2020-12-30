a5776c11e18cbbeafa835ecd05213fa72a05ed95
slowy07 <slowy.arfy@gmail.com>
2020-12-30 07:06:17 +0700

    word break (medium)

---
+++ leetCodeSolution101-200/wordBreak/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& dict) {
+        unordered_set<string> st(begin(dict), end(dict));
+        int N = s.size();
+        vector<bool> dp(N + 1);
+        dp[0] = true;
+        for (int i = 1; i <= N; ++i) {
+            for (int j = 0; j < i && !dp[i]; ++j) {
+                dp[i] = dp[j] && st.count(s.substr(j, i - j));
+            }
+        }
+        return dp[N];
+    }
+};
