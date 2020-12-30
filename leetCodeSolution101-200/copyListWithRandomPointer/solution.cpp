8ee985b87d13ff225e229b51ae564a7d1e5ff7e1
slowy07 <slowy.arfy@gmail.com>
2020-12-30 07:03:44 +0700

    copy list wth random pointer (medium)

---
+++ leetCodeSolution101-200/copyListWithRandomPointer/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (!head) {
+            return nullptr;
+        }
+        unordered_map<Node*, Node*> old_to_new;
+        Node* new_head = new Node(head->val);
+        Node *new_curr = new_head, *curr = head->next;
+        old_to_new[head] = new_head;
+        while (curr) {
+            new_curr->next = new Node(curr->val);
+            new_curr = new_curr->next;
+            old_to_new[curr] = new_curr;
+            curr = curr->next;
+        }
+        new_curr = new_head;
+        curr = head;
+        while (curr) {
+            new_curr->random = old_to_new[curr->random];
+            curr = curr->next;
+            new_curr = new_curr->next;
+        }
+        return new_head;
+    }
+};
