10ba416a6e2567026a2c05b5bd402e5cad4d50d9
arfy slowy <slowy.arfy@gmail.com>
2022-12-20 22:17:22 +0700

    Create 484.cpp

---
+++ leetCodeSolution/484.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int result = 0, local_max = 0;
+        for (const auto& n : nums) {
+            local_max = n ? local_max + 1 : 0;
+            result = max(result, local_max);
+        }
+        return result;
+    }
+};
