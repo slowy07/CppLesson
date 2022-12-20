e4cd1e40161bdd96b7226b7cf9cf09af1c2ae245
arfy slowy <slowy.arfy@gmail.com>
2022-12-20 22:20:16 +0700

    Create predict-the-winner.cpp

---
+++ leetCodeSolution/predict-the-winner.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool PredictTheWinner(vector<int>& nums) {
+        if (nums.size() % 2 == 0 || nums.size() == 1) {
+            return true;
+        }
+        vector<int> dp(nums.size());
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            dp[i] = nums[i];
+            for (int j = i + 1; j < nums.size(); ++j) {
+                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
+        }
+        return dp.back() >= 0;
+    }
+};
