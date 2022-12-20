d25f837b961b4a14fc2c7756960f4c5f4e69c58b
arfy slowy <slowy.arfy@gmail.com>
2022-12-20 21:52:39 +0700

    Create 477.cpp

---
+++ leetCodeSolution/477.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        int result = 0;
+        for (int i = 0; i < 8 * sizeof(int); ++i) {
+            vector<int> counts(2);
+            for (const auto& num : nums) {
+                ++counts[(num >> i) & 1];
+            }
+            result += counts[0] * counts[1];
+        }
+        return result;
+    }
+};
